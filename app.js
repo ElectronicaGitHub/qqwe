f457db60a34db86676ddcfabf94b3b9da09b35b9
Antonov Philip <molo4nik11@gmail.com>
2013-11-03 07:32:57 +0400

    main

---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var config = require('./config');
+var mongoose = require('./libs/mongoose');
+var log = require('./libs/log')(module);
+var passport = require('passport');
+
+require('./routes/passportfb');
+
+
+
+var app = express();
+app.set('port', config.get('port'));
+
+app.engine('ejs', require('ejs-locals'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+
+var MongoStore = require('connect-mongo')(express);
+
+app.use(express.session( {
+	secret : config.get('session:secret'),
+	key : config.get('session:key'),
+	cookie : config.get('session:cookie'),
+	store: new MongoStore({mongoose_connection : mongoose.connection})
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.use(function (req, res, next) {
+// 	req.session.numberOfVisits = req.session.numberOfVisits + 1 || 1;
+// 	res.send("Visits: " + req.session.numberOfVisits);
+// });
+
+app.use(app.router);
+
+require('./routes')(app);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(function (err, req, res, next) {
+	if (app.get('env') == 'development') {
+		var errorHandler = express.errorHandler();
+		errorHandler(err, req, res, next); 
+	} else {
+		res.send(500);
+	}
+	// error sender
+});
+
+http.createServer(app).listen(config.get('port'), function(){
+  log.info('Express server listening on port ' + config.get('port'));
+});
+
+
