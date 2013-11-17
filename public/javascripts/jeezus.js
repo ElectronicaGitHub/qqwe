5dbd0c5a8ae217243866415ca7c3401044b9325d
Antonov Philip <molo4nik11@gmail.com>
2013-11-18 00:51:32 +0400

    modernized

--- public/javascripts/jeezus.js
+++ public/javascripts/jeezus.js
@@ -24,7 +24,8 @@ $(document).ready(function() {
 
 	if ( (navigator.userAgent.match(/iPhone/i)) 
    	  || (navigator.userAgent.match(/iPod/i)) 
-      || (navigator.userAgent.match(/iPad/i)) ) 
+      || (navigator.userAgent.match(/iPad/i))
+      || (navigator.userAgent.match(/Android/i)) ) 
 		{
 
       		$(".signin").on('touchstart', function(){
