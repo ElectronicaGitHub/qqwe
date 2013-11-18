ced91e6fcd2defb7e3ec76f6a1c9aab4a75bb5b4
Antonov Philip <molo4nik11@gmail.com>
2013-11-19 00:10:59 +0400

    modernized

--- public/javascripts/jeezus.js
+++ public/javascripts/jeezus.js
@@ -24,7 +24,6 @@ $(document).ready(function() {
 
 	if ( (navigator.userAgent.match(/iPhone/i)) 
    	  || (navigator.userAgent.match(/iPod/i)) 
-      || (navigator.userAgent.match(/iPad/i))
       || (navigator.userAgent.match(/Android/i)) ) 
 		{
 
